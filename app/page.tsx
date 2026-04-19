53dbb3f42c9df96695d10ddad000723a707c7e32
pipeCode <felipe.moreno23@hotmail.es>
2026-04-18 23:50:35 -0500

    Validar variable global

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import { getGames } from '@/lib/api'
 import { HomeClient } from "./page-client";
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const [games] = await Promise.all([getGames()])
 
